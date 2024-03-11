6b255568eba5ec876eff7d3522b63942d94276f9
semilore <semilore@omotade-michaels.com>
2024-03-11 17:57:08 +0100

    feat: signup and login pages

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/signup">Signup</RouterLink>
+    </nav>
+  </header>
+  <RouterView /> -->
+  <!-- <Login /> -->
+  <Signup />
+</template>
+
+<script setup lang="ts">
+import Login from './views/Login.vue';
+import Signup from './views/Signup.vue'
+// import { RouterLink, RouterView } from 'vue-router';
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Signup from '../views/Signup.vue'
+import Login from '../views/Login.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import('../views/AboutView.vue')
+      component: Signup
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login
+    }
+    
+  ]
+})
+
+export default router
